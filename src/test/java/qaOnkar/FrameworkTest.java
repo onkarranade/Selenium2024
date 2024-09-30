010959f7d04b79b95f068f8147122d07fe3040ac
onkarranade <onkarranade1989@gmail.com>
2024-09-30 09:00:54 -0400

    franework test file added and updated pom.xml

--- src/test/java/qaOnkar/FrameworkTest.java
+++ src/test/java/qaOnkar/FrameworkTest.java
@@ -1,5 +1,10 @@
 package qaOnkar;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class FrameworkTest {
 
+	
+	WebDriver driver = new ChromeDriver();
 }
