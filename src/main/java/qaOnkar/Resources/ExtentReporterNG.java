054724ef9666102a9d7184ab1aa1f237b4007730
onkarranade <onkarranade1989@gmail.com>
2024-10-06 16:25:36 -0400

    framweork enhancements. file added for extent reports

---
+++ src/main/java/qaOnkar/Resources/ExtentReporterNG.java
@@ -0,0 +1,20 @@
+package qaOnkar.Resources;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentReporterNG {
+
+	public void getReportObject()
+	{
+		String path= System.getProperty("user.dir")+"//reports/index.html";
+		ExtentSparkReporter reporter=new ExtentSparkReporter(path);
+		reporter.config().setReportName("Automation results");
+		reporter.config().setDocumentTitle("Test Results");
+		
+		ExtentReports extent=new ExtentReports();
+		extent.attachReporter(reporter);
+		extent.setSystemInfo("QA", "Onkar");
+	}
+	
+}
